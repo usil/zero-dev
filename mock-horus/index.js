4d0a20348c02c5a989bc37334bc16e608ce0a6bc
Luis Huertas <hu3rtas@gmail.com>
2022-07-18 17:55:55 -0500

    updated

---
+++ mock-horus/index.js
@@ -0,0 +1,138 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/authorize", (req, res) => {
+  return res.json({
+    content: {
+      url: `${process.env.OWN_BASE_URL}/callback`,
+    },
+  });
+});
+
+app.post("/token_user", (req, res) => {
+  return res.status(200).json({
+    content: {
+      accessToken: "test",
+      refreshToken:
+        "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiNjRhZGQxYi05YzBhLTQyMjAtYjVhNC0zOTBkMjU1M2FiOWUiLCJpYXQiOjE2NTA1MjI3MjIsInN1YiI6InJsZW9uaUB1c2lsLmVkdS5wZSIsImV4cCI6MTY1MDUyNjMyMn0.NdLxkxyBqYM3aOVwWLkMrZYWkG8dPXB1pBvYkrRO-us",
+      tokenType: "bearer",
+      expiresIn: "3600s",
+      mail: "rleoni@usil.edu.pe",
+      displayName: "Richard Leon Ingaruca",
+      firstName: "jane",
+      lastName: "doe",
+      category: "cat1",
+      position: "pos1",
+      businessUnits: [
+        {
+          identifier: "radUnit",
+          profiles: [
+            {
+              identifier: "radProfile",
+              roles: [
+                {
+                  identifier: "asistente",
+                  options: [
+                    {
+                      identifier: "opt1",
+                      name: "opt1",
+                      description: "opt1",
+                      id: 1,
+                      type: "WEB_OPTION",
+                      value: "proyecto",
+                      order: 0,
+                    },
+                    {
+                      identifier: "opt1",
+                      name: "opt1",
+                      description: "opt1",
+                      id: 1,
+                      type: "WEB_OPTION",
+                      value: "servicio",
+                      order: 0,
+                    },
+                    {
+                      identifier: "opt1",
+                      name: "opt1",
+                      description: "opt1",
+                      id: 1,
+                      type: "WEB_OPTION",
+                      value: "plan_mensual",
+                      order: 0,
+                    },
+                    {
+                      identifier: "opt2",
+                      name: "opt2",
+                      description: "opt2",
+                      id: 2,
+                      type: "INTERNAL_RULE",
+                      value: "proyecto::CRUD",
+                      order: 4,
+                    },
+                    {
+                      identifier: "opt2",
+                      name: "opt2",
+                      description: "opt2",
+                      id: 2,
+                      type: "INTERNAL_RULE",
+                      value: "servicio::CRUD",
+                      order: 4,
+                    },
+                    {
+                      identifier: "opt2",
+                      name: "opt2",
+                      description: "opt2",
+                      id: 2,
+                      type: "INTERNAL_RULE",
+                      value: "plan_mensual::CRUD",
+                      order: 4,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  });
+});
+
+app.get("/callback", (req, res) => {
+  return res
+    .status(302)
+    .redirect(`${process.env.REDIRECT_FRONT_BASE_URL}/oauth2/callback`);
+});
+
+app.post("/v2/oauth2/token/validate", (req, res) => {
+  if (req.body.token === "test") {
+    return res.json({
+      code: 200000,
+      message: "success",
+      content: {
+        isAllowed: true,
+        subject: "sanvi@828fligth.com",
+      },
+    });
+  }
+  return res.json({
+    code: 200000,
+    message: "success",
+    content: {
+      isAllowed: false,
+      subject: "sanvi@828fligth.com",
+    },
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listening ready");
+});
---
+++ one_click.sh
@@ -0,0 +1,105 @@
+workspaces_path=$(pwd)
+
+function delete_image_if_exist {
+  image=$(docker images "$1" -a -q)
+  if [ ! "$image" == "" ]; then
+    echo "deleting image: $image"
+    docker rmi -f "$image"
+  fi
+}
+
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+   export "$KEY"="$VALUE"
+done
+
+echo "#################"
+echo "Updating source code"
+echo "#################"
+
+cd $workspaces_path/eventhos-api
+git fetch
+latest_branch=$(git for-each-ref --sort=-committerdate | head -n 1 | awk -F '/' '{ print $NF }')
+git checkout $latest_branch
+git pull origin $latest_branch
+
+cd $workspaces_path/eventhos-web
+git fetch
+latest_branch=$(git for-each-ref --sort=-committerdate | head -n 1 | awk -F '/' '{ print $NF }')
+git checkout $latest_branch
+git pull origin $latest_branch
+
+
+echo "#################"
+echo "Launching"
+echo "#################"
+cd $workspaces_path
+
+export JWT_SECRET=$(uuidgen)
+export CRYPTO_KEY=$(uuidgen)
+export MYSQL_PASSWORD=$(uuidgen)
+
+if [ ! -f .env ]; then
+  echo "" > .env
+else
+  if [ -s .env ]; then
+    echo ".env is empty"
+  else
+    export $(cat .env | xargs)
+  fi
+fi
+
+if [[ -z "$ZERO_CODE_API" ]]
+then
+  echo "Enter ZERO_CODE_API: http://localhost:2111, http://10.20.30.40:2111 or https://eventhos.foo.com"
+  read _ZERO_CODE_API
+  export ZERO_CODE_API=$_ZERO_CODE_API
+  echo "ZERO_CODE_API=$ZERO_CODE_API" >> .env
+fi
+
+if [[ -z "$SECURITY_OAUTH2_BASE_URL" ]]
+then
+  echo "Enter EVENTHOS_WEB_BASE_URL: (The mock server) "
+  read _SECURITY_OAUTH2_BASE_URL
+  export SECURITY_OAUTH2_BASE_URL=$_SECURITY_OAUTH2_BASE_URL
+  echo "SECURITY_OAUTH2_BASE_URL=$SECURITY_OAUTH2_BASE_URL" >> .env
+fi
+
+if [[ -z "$CUSTOM_SECURITY_BASE_URL" ]]
+then
+  echo "Enter CUSTOM_SECURITY_BASE_URL: (2109) "
+  read _CUSTOM_SECURITY_BASE_URL
+  export CUSTOM_SECURITY_BASE_URL=$_CUSTOM_SECURITY_BASE_URL
+  echo "CUSTOM_SECURITY_BASE_URL=$CUSTOM_SECURITY_BASE_URL" >> .env
+fi
+
+if [[ -z "$REDIRECT_FRONT_BASE_URL" ]]
+then
+  echo "Enter REDIRECT_FRONT_BASE_URL: (The url to the front end of the rad (2900)) "
+  read _REDIRECT_FRONT_BASE_URL
+  export REDIRECT_FRONT_BASE_URL=$_REDIRECT_FRONT_BASE_URL
+  echo "REDIRECT_FRONT_BASE_URL=$REDIRECT_FRONT_BASE_URL" >> .env
+fi
+
+if [[ -z "$OWN_BASE_URL" ]]
+then
+  echo "Enter OWN_BASE_URL: (The comple base url to the horus mock itself) "
+  read _OWN_BASE_URL
+  export OWN_BASE_URL=$_OWN_BASE_URL
+  echo "OWN_BASE_URL=$OWN_BASE_URL" >> .env
+fi
+
+# https://stackoverflow.com/a/50850881
+
+if [ "$build" == "true" ]; then
+  docker-compose down -v
+  delete_image_if_exist "zero-dev"
+  delete_image_if_exist "zero-code-api"
+  delete_image_if_exist "mock-horus"
+  docker-compose up -d --build
+else
+  docker-compose down && docker-compose up -d
+fi
